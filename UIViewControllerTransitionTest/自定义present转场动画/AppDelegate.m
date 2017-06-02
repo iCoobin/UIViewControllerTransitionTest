3b03e3c924d62c50b116d19688b5aaab0366df5d
iCoobin <15757117989@163.com>
2017-06-02 11:54:19 +0800

    add no change .. for AppDelegate

--- UIViewControllerTransitionTest/自定义present转场动画/AppDelegate.m
+++ UIViewControllerTransitionTest/自定义present转场动画/AppDelegate.m
@@ -19,6 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     //change ..
+    // no change ..
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
 //    self.window.backgroundColor = [UIColor whiteColor];
